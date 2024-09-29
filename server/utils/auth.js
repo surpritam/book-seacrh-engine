47c97838d0a64ebecc5379f6ad00a607b1370951
Pritam Sur <pritamsur@Pritams-MacBook-Pro-2.local>
2024-09-29 18:48:47 -0400

    update auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,32 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
+// Set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
+// Function to authenticate and extract user from token
+const getUserFromToken = (token) => {
+  try {
+    if (token) {
+      // Remove "Bearer " from the token string
+      const actualToken = token.split(' ').pop().trim();
+      const { data } = jwt.verify(actualToken, secret, { maxAge: expiration });
+      return data;
+    }
+    return null;
+  } catch (error) {
+    console.log('Invalid token:', error);
+    return null;
+  }
+};
+
+// Function to sign a new token
+const signToken = ({ username, email, _id }) => {
+  const payload = { username, email, _id };
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+};
+
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
-
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  getUserFromToken,
+  signToken,
 };
